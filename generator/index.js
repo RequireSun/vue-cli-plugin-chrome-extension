70b1daf8990b25f5cd21f029de4056cb1a495e2c
RequireSun <kelvinlpsun@hotmail.com>
2018-11-19 01:44:01 +0800

    干他妈的傻逼 vue-cli，傻逼 node.js，cli 默认生成的模板文件干不掉，fs.unlink 告诉我成功了实际上什么也没做

--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,32 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (api, options, rootOptions) => {
     api.extendPackage({
         devDependencies: {
-            "copy-webpack-plugin": "latest",
+            "copy-webpack-plugin": "^4.0.0",
         },
     });
 
     api.render('./template');
+
+    const indexPath = path.resolve(process.cwd(), 'public' + path.sep + 'index.html');
+
+    fs.lstat(indexPath, (...args) => {
+        if (args[0]) {
+            // do nothing
+        }
+        console.log('lstat', args);
+    });
+
+    fs.unlink(indexPath, (err) => {
+        if (err) {
+            // do nothing
+            console.log('remove file fail', err);
+        } else {
+            console.log('remove file success', indexPath);
+        }
+    });
 };
